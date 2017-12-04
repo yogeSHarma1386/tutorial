83d7c02d4ac4237290c8bd00c4d2a3fe6b62d388
YogeSHarma <yogesh.sharma@hashedin.com>
2017-12-05 01:43:29 +0530

    Serializers --with api_view

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from . import views
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/(?P<pk>[0-9]+)$', views.snippet_detail),
     url(r'^admin/', admin.site.urls)
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
