eeade628836f0c760465da41a9e1e7a0da26a32b
YogeSHarma <yogesh.sharma@hashedin.com>
2017-12-05 05:03:28 +0530

    Correcting the class based links

--- snippets/views/__init__.py
+++ snippets/views/__init__.py
@@ -5,8 +5,8 @@ func_snippet_detail = function_based.snippet_detail
 func_snippet_list = function_based.snippet_list
 
 # Step2
-klass_snippet_detail = class_based.SnippetList.as_view()
-klass_snippet_list = class_based.SnippetDetail.as_view()
+klass_snippet_detail = class_based.SnippetDetail.as_view()
+klass_snippet_list = class_based.SnippetList.as_view()
 
 # Step3
 mixin_snippet_detail = mixin_based.SnippetDetail.as_view()
