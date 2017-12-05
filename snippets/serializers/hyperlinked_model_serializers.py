6cae482580d071776823b20d18737aa3d771ec09
YogeSHarma <yogesh.sharma@hashedin.com>
2017-12-06 02:30:20 +0530

    Specifically mentioning serializer-level:view_name(s)

--- snippets/serializers/hyperlinked_model_serializers.py
+++ snippets/serializers/hyperlinked_model_serializers.py
@@ -6,6 +6,7 @@ from ..models import Snippet
 
 class SnippetHyperLinkedSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    # This view_name is field-level.
     highlight = serializers.HyperlinkedIdentityField(view_name='h-snippet-highlight', format='html')
 
     class Meta:
@@ -13,10 +14,21 @@ class SnippetHyperLinkedSerializer(serializers.HyperlinkedModelSerializer):
         fields = ('url', 'id', 'highlight', 'owner',
                   'title', 'code', 'linenos', 'language', 'style')
 
+        # Mentioning serializer-level:view_name explicitly
+        extra_kwargs = {
+            'url': {'view_name': 'snippet-detail'}
+        }
+
 
 class UserHyperLinkedSerializer(serializers.HyperlinkedModelSerializer):
+    # This view_name is field-level.
     snippets = serializers.HyperlinkedRelatedField(many=True, view_name='h-snippet-detail', read_only=True)
 
     class Meta:
         model = User
         fields = ('url', 'id', 'username', 'snippets')
+
+        # Mentioning serializer-level:view_name explicitly
+        extra_kwargs = {
+            'url': {'view_name': 'user-detail'}
+        }
